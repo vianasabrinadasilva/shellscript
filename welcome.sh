3e6c4fb0972dd0442219c0c9a4e5ee9dbec7e466
sabrina <sabrinadasilvav@gmail.com>
2018-04-24 17:03:12 -0300

    Modificado o arquivo welcome.sh, acrescentado estrutura if

--- welcome.sh
+++ welcome.sh
@@ -2,4 +2,16 @@
 clear;
 echo -n "Digite seu nome: ";
 read nome;
-echo "Hello $nome"
+echo -n "Digite a sua idade: ";
+read idade;
+clear;
+echo "_________________"
+echo "Nome:" $nome
+echo "Idade:" $idade
+if [ $idade -lt 18 ]; then
+echo "Menor de idade";
+else
+echo "Maior de idade";
+fi 
+echo "__________________"
+exit;
